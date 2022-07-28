969970722da4131e8eaaff9f684fadc1929105a9
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-07-28 10:32:56 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [events, setEvents] = useState([
+    {title:"Steve doing this finally", id: 1},
+    {title:"Upskilling is the goal", id: 2},
+    {title:"I will complete this course", id: 3}
+  ])
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      })
+    })
+    console.log(id)
+  }
+
+  return(
+    <div className='App'>
+      {events.map((event, index) =>
+        <div key={event.id}>
+          <h2>{index} - {event.id} -  {event.title}</h2>
+         <button onClick={() => handleClick(event.id)}> delete event</button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
