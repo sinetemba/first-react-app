d220f0db1499a857b208150ae5d58d09fbcb83de
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-07-29 14:03:24 +0200

    conditional templates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //1. hooks must always be called at the top level of the component 
+  //2. must be using within a scope of the react component
+  const [showEvents, setShowEvents] = useState(true) //boolean state
   const [events, setEvents] = useState([
     {title:"Steve doing this finally", id: 1},
     {title:"Upskilling is the goal", id: 2},
     {title:"I will complete this course", id: 3}
   ])
 
+  console.log(showEvents);
   const handleClick = (id) => {
     setEvents((prevEvents) => {
       return prevEvents.filter((event) => {
@@ -19,7 +23,20 @@ function App() {
 
   return(
     <div className='App'>
-      {events.map((event, index) =>
+
+      {showEvents && (
+      <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+      </div>
+      )}
+
+      {!showEvents && (
+      <div>
+        <button onClick={() => setShowEvents(true)}>show events</button>
+      </div>
+       )}
+
+      {showEvents && events.map((event, index) =>
         <div key={event.id}>
           <h2>{index} - {event.id} -  {event.title}</h2>
          <button onClick={() => handleClick(event.id)}> delete event</button>
