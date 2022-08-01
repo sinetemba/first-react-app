aa38ca6df3567769eab40553ca7d1a32fc853e0a
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-08-01 14:10:18 +0200

    before useRefHook

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import NewEventForm from './components/NewEventForm'
 function App() {  
   const [showModal, setShowModal] = useState(false)
   const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([
-    {title:"Steve doing this finally", id: 1},
-    {title:"Upskilling is the goal", id: 2},
-    {title:"I will complete this course", id: 3}
-  ])
-  console.log(showModal);
+  const [events, setEvents] = useState([])
+
+  const addEvent = (event) =>{
+    setEvents((prevEvents) => {
+      return [...prevEvents, event]
+    })
+
+    setShowModal(false)
+  }
 
   const handleClick = (id) => {
     setEvents((prevEvents) => {
@@ -23,12 +26,7 @@ function App() {
     })
     console.log(id)
   }
-
- 
-  const handleClose = () => {
-    setShowModal(false)
-  }
-
+  
   const subtitleConst = "All the latest events in Marioland"
 
   return(
@@ -51,8 +49,8 @@ function App() {
       {showEvents && <EventList events={events} handleClick={handleClick} />}
 
    
-       {showModal && <Modal handleClose={handleClose} isSalesModal={true}>
-          <NewEventForm/>
+       {showModal && <Modal isSalesModal={true}>
+          <NewEventForm addEvent={addEvent} />
         </Modal>
        }
 
--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -2,25 +2,41 @@ import React from 'react'
 import './NewEventForm.css'
 import {useState} from 'react';
 
-export default function NewEventForm() {
+export default function NewEventForm({addEvent}) {
 
   const [title, setTitle] = useState('')
   const [date, setDate] = useState('')
 
+  const resetForm = () =>{
+    setTitle('')
+    setDate('')
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    const event = {
+        title: title,
+        date: date,
+        id: Math.floor(Math.random() * 10000)
+    }
+    addEvent(event)
+    resetForm()
+  }
+
 
   return (
-    <form className="new-event-form">
+    <form className="new-event-form" onSubmit={handleSubmit}>
         <label>
             <span>Event Title:</span> 
-            <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+            <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
         </label>
 
         <label>
             <span>Event Date:</span> 
-            <input type="date" onChange={(e) =>setDate(e.target.value)}/>
+            <input type="date" onChange={(e) =>setDate(e.target.value)} value={date}/>
         </label>
-        <button>Submit</button>
-        <p>title - {title}, date - {date} </p>
+        <button>Submit</button>      
     </form>
   )
 }
